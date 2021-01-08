91a579bf29d124388f9bcbc840b7fb29772c0f06
baoqiang <zmrnet@qq.com>
2021-01-08 13:32:29 +0800

    gohttpd

--- toolkit/gohttpd/x_win32.bat
+++ toolkit/gohttpd/x_win32.bat
@@ -6,6 +6,7 @@ set TARGET=gohttpd
 
 go fmt %TARGET%
 
-go build -o %TARGET%.exe -ldflags "-H windowsgui -w -s" %TARGET%
+: go build -o %TARGET%.exe -ldflags "-H windowsgui -w -s" %TARGET%
+go build -o %TARGET%.exe -ldflags "-w -s" %TARGET%
 
 @pause
